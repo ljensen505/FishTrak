5b51006d4c5dbb0eebff8e72a7c2a02b3ef989ef
lucas <lucas.pjensen10@gmail.com>
2022-04-30 17:14:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FishTrak o'Matic
+FishTrack o'Matic is currently underdevelopment. It is being build by Jerrod Lepper and Lucas Jensen.
---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+The main UI file for FishTrak o'Matic
+Lucas Jensen
+Jerrod Lepper
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """
+    The route for the homepage
+    """
+    return render_template('index.html')
+
+
+def main():
+    """
+    the main function for running the UI
+    """
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
