674fcc3b653cdf243971fca200cf9c88e4406a60
lucas <lucas.pjensen10@gmail.com>
2022-05-01 20:41:27 -0700

    Started Update for fishermen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ The main UI file for FishTrak o'Matic
 Lucas Jensen
 Jerrod Lepper
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,13 +17,23 @@ def home():
 @app.route('/fishermen')
 def fishermen():
     """The route for displaying all fishermen"""
-    return render_template('fishermen.html', title='All Fishermen')
+    people = ['Fake Jerrod', 'Fake Lucas']
+    return render_template('fishermen.html', title='All Fishermen', people=people)
 
 
-@app.route('/add_fisherman')
+@app.route('/add_fisherman', methods=['GET', 'POST'])
 def add_fisherman():
     """add a fisherman to the db"""
+    if request.method == 'POST':
+        print(f"You added {request.form['name']} to the db! (not really)")
     return render_template('add_fisherman.html', title='Add Fisherman')
+
+
+@app.route('/fishermen/<name>', methods=['GET', 'POST'])
+def update_fisherman(name):
+    """updates a specified fisherman"""
+    return render_template('update_fisherman.html', title='Update Fisherman', name=name)
+
 
 
 def main():
